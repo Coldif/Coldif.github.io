f9b758fd2ddd643f466a1da0c49402fb45cfdc8b
Coldif <ocoldif@gmail.com>
2024-08-31 20:58:27 -0300

    .

--- proxy.js
+++
@@ -1,7 +0,0 @@
-function FindProxyForURL(url, host) {
-  if (shExpMatch(host, "*discord*") || shExpMatch(host, "*wa.me*") || shExpMatch(host, "*whatsapp.com*") || shExpMatch(host, "*facebook.com*") ||  shExpMatch(host, "*dropbox.com*") || shExpMatch(host, "*onedrive.com*"))
-    return "DIRECT";
-  if (shExpMatch(host, "prod.qualplacar.top*"))
-    return "PROXY 152.70.217.36:8899";
-  return "DIRECT";
-}
